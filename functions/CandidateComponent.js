01eacba056b9f664b27f3c488f337405f955bf6e
JhormanMera <1010159945@u.icesi.edu.co>
2022-05-22 07:21:10 -0500

    Creating the components

---
+++ functions/CandidateComponent.js
@@ -0,0 +1,17 @@
+class CandidateComponent{
+
+    constructor(candidate){
+        this.candidate=candidate;
+    }
+
+    render(container){
+        let html=`
+        
+        `;
+
+    }
+
+    action(event){
+
+    }
+}
