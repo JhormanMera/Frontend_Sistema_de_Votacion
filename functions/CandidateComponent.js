26a14791e0ad24554e905ff633daabdf1b752d7b
JhormanMera <1010159945@u.icesi.edu.co>
2022-05-22 08:08:17 -0500

    Modelating the action event

--- functions/CandidateComponent.js
+++ functions/CandidateComponent.js
@@ -12,6 +12,24 @@ class CandidateComponent{
     }
 
     action(event){
-
+        event.preventDefault();
+        let xhr = new XMLHttpRequest();
+        xhr.open('PUT','***************************************************************');
+        xhr.setRequestHeader('Content-Type','application/json');
+        xhr.send(JSON.stringify(this.candidate) );
+        window.alert('Voto registrado exitosamente');
+        window.location.href = "results.html";
     }
+
+    /* action(event){
+        event.preventDefault();
+        let url = `*********************************************************`;
+        fetch(url, {method:'PUT'})
+        .then(response => response.json())
+        .then(data => {
+            alert('Voto registrado exitosamente');
+            window.location.href = "results.html";
+        })
+        .catch(error => console.log(error));
+    }*/
 }
